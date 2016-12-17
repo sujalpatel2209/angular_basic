4c4d786bf66aa1c3e33f53de29f4d96618fc844e
sujalpatel2209 <sujalpatel022@gmail.com>
2016-12-17 00:13:00 -0800

    create new component and use in another component using nested component

---
+++ project1/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/index';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  directives: [HomeComponent]
+})
+export class AppComponent {
+  title = 'Angular JS Page';
+  name = 'Sujal Patel';
+}
