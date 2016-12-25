a2b7c9ee3e9ed61bb56f6181182b1b13bf9074c1
sujalpatel2209 <sujalpatel022@gmail.com>
2016-12-25 04:13:59 -0800

    ~ create lis using ngFor ~ print array list

--- market-app/src/app/market/market.component.ts
+++ market-app/src/app/market/market.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-market',
-  templateUrl: './market.component.html',
-  styleUrls: ['./market.component.css']
+selector: 'app-market',
+templateUrl: './market.component.html',
+styleUrls: ['./market.component.css']
 })
 export class MarketComponent implements OnInit {
 
-  constructor() { }
+marketItem = [
+{ desc : 'Book1', type: 'B1' },
+{ desc : 'Book2', type: 'B2' },
+{ desc : 'Book3', type: 'B3' },
+{ desc : 'Book4', type: 'B4' },
+];
+    
+addMarketItem(){
+    alert('Hello');
+}
+constructor() { }
 
-  ngOnInit() {
-  }
+ngOnInit() {
+}
 
 }
